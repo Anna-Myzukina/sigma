517fcf4f0bf03ee2ae7e79e07e4690d955a88917
Anna Myzukina <anna.muzykina83@gmail.com>
2019-12-26 23:35:12 +0200

    Add a Static Pages controller

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #checklist" do
+    it "returns http success" do
+      get :checklist
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
